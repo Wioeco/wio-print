56d8146fac68417d3e98ea16946fc81164d140e4
Wioeco <wioeco@gmail.com>
2025-06-17 16:27:33 +0200

    Add files via upload

---
+++ print.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+import pandas as pd
+from datetime import datetime, timezone
+import pytz
+import ast
+import json
+
+print(f"Albarán:}")
+
